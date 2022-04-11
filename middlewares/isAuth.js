f17f7deaebd298332ad396016cf7f3063d9cb06d
hoangdv99 <hoangdv257@gmail.com>
2022-04-11 23:20:02 +0700

    :hammer: Fix authentication

--- middlewares/isAuth.js
+++ middlewares/isAuth.js
@@ -1,21 +1,14 @@
+
 import pkg from 'jsonwebtoken'
-const { verify } = pkg;
+const { verify } = pkg
 
 export const authMiddleware = (req, res, next) => {
-  const userToken = req.headers['x-access-token']
-  if (!userToken) {
-    return res.status(401).send('No token provided')
-  }
-  let decodedToken
-  try {
-    decodedToken = verify(userToken, process.env.ACCESS_TOKEN_SECRET)
+  const token = req.body.token || req.query.token || req.headers['x-access-token'] || ''
+  if (!token) next()
+  else {
+    const decodedToken = verify(token, process.env.ACCESS_TOKEN_SECRET)
     req.user = decodedToken
-  } catch (err) {
-    err.statusCode = 500
-    throw err
+
+    next()
   }
-  if (!decodedToken) {
-    return res.status(401).send('No token provided')
-  }
-  next()
 }
