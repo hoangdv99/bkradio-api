2da014ebb1dba1af7569e84c062ae7f8db705ddf
hoangdv99 <hoangdv257@gmail.com>
2022-04-24 10:03:06 +0700

    :+1: Search feature

--- middlewares/isAuth.js
+++ middlewares/isAuth.js
@@ -3,9 +3,10 @@ import pkg from 'jsonwebtoken'
 const { verify } = pkg
 
 export const authMiddleware = (req, res, next) => {
-  const token = req.body.token || req.query.token || req.headers['x-access-token'] || ''
+  let token = req.body.token || req.query.token || req.headers['x-access-token'] || ''
   if (!token) next()
   else {
+    token = token.replace('Bearer ', '')
     const decodedToken = verify(token, process.env.ACCESS_TOKEN_SECRET)
     req.user = decodedToken
 
