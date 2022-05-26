903e85d1685050b22d2999c0b46f70d0a3d231e3
hoangdv99 <hoangdv257@gmail.com>
2022-05-26 23:12:39 +0700

    :tada: Add CICD

--- knexfile.js
+++ knexfile.js
@@ -12,11 +12,10 @@ attachPaginate()
 export default knex({
   client: 'mysql',
   connection: {
+    socketPath: process.env.MYSQL_SOCK,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_NAME,
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
     typeCast: function (field, next) {
       if (field.type === 'JSON') {
         return (JSON.parse(field.string()))
