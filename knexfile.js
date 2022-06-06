54c769efc70d34920ce5c2541ba5acccaa6c955e
hoangdv99 <hoangdv257@gmail.com>
2022-06-06 22:12:58 +0700

    :hammer: Connect to upcloud db

--- knexfile.js
+++ knexfile.js
@@ -12,7 +12,8 @@ attachPaginate()
 export default knex({
   client: 'mysql',
   connection: {
-    socketPath: process.env.MYSQL_SOCK,
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_NAME,
