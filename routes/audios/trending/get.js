bee2fd8e2ee8c5bd89e84afd215821515c56124b
hoangdv99 <hoangdv257@gmail.com>
2022-07-03 17:23:06 +0700

    :+1: Improve getting trending

--- routes/audios/trending/get.js
+++ routes/audios/trending/get.js
@@ -4,19 +4,21 @@ import { audioStatus } from '../../../constants'
 
 export default async (req, res) => {
   const period = req.query.period
-  if (period === 'all') {
-    const audios = await knex.select(
-      'id',
-      'title',
-      'author',
-      'thumbnail_url',
-      'slug'
-    )
-      .from('audios')
-      .where({ status: audioStatus.public })
-      .orderBy('views', 'desc')
-      .limit(10)
+  const audios = await knex.select(
+    'a.id',
+    'a.title',
+    'a.author',
+    'a.thumbnail_url',
+    'a.slug',
+  )
+    .count('a.id', { as: 'listen_time_count' })
+    .from('audios as a')
+    .where({ status: audioStatus.public })
+    .join('histories as h', 'a.id', '=', 'h.audio_id')
+    .where('h.listened_percent', '>=', 10)
+    .groupBy('a.id')
+    .orderBy('listen_time_count', 'desc')
+    .limit(10)
 
-    return res.status(200).send(Object.values(audios).map(audio => camelize(audio)))
-  }
+  return res.status(200).send(Object.values(audios).map(audio => camelize(audio)))
 }
