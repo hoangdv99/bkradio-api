373c67c9ec98d9db6c748040dbab47c24cb9f688
hoangdv99 <hoangdv257@gmail.com>
2022-08-10 00:14:11 +0700

    :hammer: Fix getting audios by voice

--- routes/audios/get.js
+++ routes/audios/get.js
@@ -10,7 +10,6 @@ export default async (req, res) => {
     'a.title',
     'a.slug',
     'a.description',
-    'a.url',
     'a.thumbnail_url',
     'a.rating',
     'a.views',
@@ -25,8 +24,9 @@ export default async (req, res) => {
   ).from('audios as a')
     .leftJoin('audio_topic as at', 'a.id', 'at.audio_id')
     .leftJoin('topics as t', 'at.topic_id', 't.id')
-    .leftJoin('voices as v', 'a.voice_id', 'v.id')
     .leftJoin('users as u', 'a.posted_by', 'u.id')
+    .leftJoin('audio_links as links', 'links.audio_id', 'a.id')
+    .leftJoin('voices as v', 'links.voice_id', 'v.id')
     .modify(function (queryBuilder) {
       if (topic) queryBuilder.where('t.slug', '=', topic)
       if (voice) queryBuilder.where('v.slug', '=', voice)
